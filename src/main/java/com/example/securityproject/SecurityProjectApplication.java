dbbe7ecb805bbf07cf0eb82763c9c485eea9fbf5
akmanova.elizaveta <liza17ak@gmail.com>
2023-10-26 19:54:26 +0300

    nitial commit

--- src/main/java/com/example/securityproject/SecurityProjectApplication.java
+++ src/main/java/com/example/securityproject/SecurityProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecurityProjectApplication {
+	//check please
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityProjectApplication.class, args);
